11a2eb861566a34e86a1e0e6a57fbcb419775ed4
Gose <niabc70@gmail.com>
2022-08-14 19:55:47 +0900

    JS : 토글 버튼 간소화

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,12 @@
 const $nav_filter = document.querySelector(".nav_filter");
 const $filter_display = document.querySelector("#filter_display");
 
-$nav_filter.addEventListener("click", () => {
+const nav_filter_toggle = () => {
   if ($filter_display.style.display == "none") {
     $filter_display.style.display = "block";
   } else {
     $filter_display.style.display = "none";
   }
-});
+};
+
+// $nav_filter.addEventListener("click", nav_filter_toggle);
