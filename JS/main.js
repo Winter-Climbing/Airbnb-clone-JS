fe3874f10a2009f078393c7002e29f33036ae92f
Gose <niabc70@gmail.com>
2022-08-15 21:10:40 +0900

    JS : 필터 요소 밖 선택 시 사라지기

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,27 @@
 const $nav_filter = document.querySelector(".nav_filter");
 const $filter_display = document.querySelector("#filter_display");
+const $filter_contents = document.querySelector(".filter_contents");
+const $filter = document.querySelector("#filter");
+
+// filter 선택시 filter 보이기
 
 const nav_filter_toggle = () => {
-  if ($filter_display.style.display == "none") {
+  let filter_boolean = false;
+
+  if ($filter_display.style.display === "none") {
     $filter_display.style.display = "block";
+    filter_boolean = true;
+    // 요소 밖 선택시 filter 지우기
+    if (filter_boolean === true) {
+      $filter_display.addEventListener("click", (e) => {
+        if (e.target === $filter) {
+          $filter_display.style.display = "none";
+          filter_boolean = false;
+        }
+      });
+    }
   } else {
     $filter_display.style.display = "none";
+    filter_boolean = false;
   }
 };
